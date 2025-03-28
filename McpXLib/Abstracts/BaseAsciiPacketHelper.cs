6898ce8f3af0829f55f11402fd9b2c6c78724a4a
YudaiKitamura <y.kitamura.eng@gmail.com>
2025-03-28 23:36:32 +0900

    ビット単位の一括読出しコマンドを実装

--- McpXLib/Abstracts/BaseAsciiPacketHelper.cs
+++ McpXLib/Abstracts/BaseAsciiPacketHelper.cs
@@ -39,7 +39,7 @@ public abstract class BaseAsciiPacketHelper : I3EResponseFrame
             stationNumber: Convert.ToByte(Encoding.ASCII.GetString(bytes.Skip(12).Take(2).ToArray()), 16)
         );
 
-        var length = Convert.ToUInt16(Encoding.ASCII.GetString(bytes.Skip(14).Take(4).ToArray()), 16) - 4;
+        var contentLength = Convert.ToUInt16(Encoding.ASCII.GetString(bytes.Skip(14).Take(4).ToArray()), 16) - 4;
 
         errCode = Convert.ToUInt16(Encoding.ASCII.GetString(bytes.Skip(18).Take(4).ToArray()), 16);
 
@@ -48,15 +48,24 @@ public abstract class BaseAsciiPacketHelper : I3EResponseFrame
             throw new McProtocolException($"An error code was received from PLC. ({ErrCode})");
         }
 
-        var asciiHex = Encoding.ASCII.GetString(bytes.Skip(22).Take(length).ToArray());
-        content = DeviceConverter.ReverseByTwoBytes(Enumerable.Range(0, asciiHex.Length / 2)
-            .Select(i => Convert.ToByte(asciiHex.Substring(i * 2, 2), 16))
-            .ToArray()
-        );
+        content = GetContent(bytes, contentLength);
+        ValidateContentLength(contentLength);
+    }
 
-        if (content.Length != length / 2) 
+    public virtual void ValidateContentLength(int contentLength)
+    {
+        if (content.Length != contentLength / 2) 
         {
             throw new RecivePacketException("Received packet had an invalid content.");
         }
     }
+
+    public virtual byte[] GetContent(byte[] bytes, int contentLength)
+    {
+        var asciiHex = Encoding.ASCII.GetString(bytes.Skip(22).Take(contentLength).ToArray());
+        return DeviceConverter.ReverseByTwoBytes(Enumerable.Range(0, asciiHex.Length / 2)
+            .Select(i => Convert.ToByte(asciiHex.Substring(i * 2, 2), 16))
+            .ToArray()
+        );
+    } 
 }
