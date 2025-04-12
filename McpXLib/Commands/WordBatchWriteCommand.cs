245dc3ef7dd93702bd40e4a83a71eea7c9b9a5eb
YudaiKitamura <y.kitamura.eng@gmail.com>
2025-04-12 13:09:11 +0900

    ワード一括書き込み、読み込みコマンドの4Eフレーム対応

--- McpXLib/Commands/WordBatchWriteCommand.cs
+++ McpXLib/Commands/WordBatchWriteCommand.cs
@@ -47,34 +47,34 @@ public sealed class WordBatchWriteCommand<T> : IPlcCommand<bool>
 
     public async Task<bool> ExecuteAsync(IPlc plc)
     {
-        if (plc.IsAscii) 
-        {
-            return new ResponseAsciiPacketParser(
-                await plc.RequestAsync(GetPacketBuilder().ToAsciiBytes())
-            ).errCode == 0;
-        }
-        else 
-        {
-            return new ResponsePacketParser(
-                await plc.RequestAsync(GetPacketBuilder().ToBinaryBytes())
-            ).errCode == 0;
-        }
+        var requestFrameSelector = new RequestFrameSelector(plc, commandPacketBuilder);
+        var responseFrameSelector = new ResponseFrameSelector(
+            plc,
+            requestFrameSelector.GetSerialNumber(),
+            DeviceAccessMode.Word
+        );
+
+        responseFrameSelector.ParsePacket(
+            await plc.RequestAsync(requestFrameSelector.GetRequestPacket())
+        );
+
+        return true;
     }
 
     public bool Execute(IPlc plc)
     {
-        if (plc.IsAscii) 
-        {
-            return new ResponseAsciiPacketParser(
-                plc.Request(GetPacketBuilder().ToAsciiBytes())
-            ).errCode == 0;
-        }
-        else 
-        {
-            return new ResponsePacketParser(
-                plc.Request(GetPacketBuilder().ToBinaryBytes())
-            ).errCode == 0;
-        }
+        var requestFrameSelector = new RequestFrameSelector(plc, commandPacketBuilder);
+        var responseFrameSelector = new ResponseFrameSelector(
+            plc,
+            requestFrameSelector.GetSerialNumber(),
+            DeviceAccessMode.Word
+        );
+
+        responseFrameSelector.ParsePacket(
+            plc.Request(requestFrameSelector.GetRequestPacket())
+        );
+
+        return true;
     }
 
     public byte[] ToBinaryBytes()
