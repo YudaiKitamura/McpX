91a1d7dbf45b3cc98fa9fa7c6d2920a5066de0b0
YudaiKitamura <y.kitamura.eng@gmail.com>
2025-04-12 12:51:38 +0900

    フレーム別のパーサークラスを実装

---
+++ McpXLib/Parsers/E3FramePacketParser.cs
@@ -0,0 +1,95 @@
+using McpXLib.Abstructs;
+using McpXLib.Enums;
+using McpXLib.Interfaces;
+
+namespace McpXLib.Parsers;
+
+public class E3FramePacketParser : IPacketParser
+{
+    private SubHeaderPacketParser subHeaderPacketParser;
+    private RoutePacketParser routePacketParser;
+    private ContentLengthPacketParser contentLengthPacketParser;
+    private ErrorCodePacketParser errorCodePacketParser;
+    private BaseContentPacketParser? contentPacketParser;
+
+    private E3FramePacketParser(IPlc plc)
+    {
+        subHeaderPacketParser = new SubHeaderPacketParser(
+            requestFrame: plc.RequestFrame,
+            isAscii: plc.IsAscii
+        );
+
+        routePacketParser = new RoutePacketParser(
+            prevPacketParser: subHeaderPacketParser,
+            isAscii: plc.IsAscii
+        );
+
+        contentLengthPacketParser = new ContentLengthPacketParser(
+            prevPacketParser: routePacketParser,
+            isAscii: plc.IsAscii
+        );
+
+        errorCodePacketParser = new ErrorCodePacketParser(
+            prevPacketParser: contentLengthPacketParser,
+            isAscii: plc.IsAscii
+        );
+    }
+
+    public E3FramePacketParser(IPlc plc, DeviceAccessMode deviceAccessMode = DeviceAccessMode.Word) : this (
+        plc: plc
+    )
+    {
+        switch (deviceAccessMode) 
+        {
+            default:
+                contentPacketParser = new WordContentPacketParser(
+                    prevPacketParser: errorCodePacketParser,
+                    isAscii: plc.IsAscii
+                );
+                break;
+            
+            case DeviceAccessMode.Bit:
+                contentPacketParser = new BitContentPacketParser(
+                    prevPacketParser: errorCodePacketParser,
+                    isAscii: plc.IsAscii
+                );
+                break;
+        }
+    }
+
+    public E3FramePacketParser(IPlc plc, int wordLength, int doubleWordLength) : this (
+        plc: plc
+    )
+    {
+        contentPacketParser = new RandomContentPacketParser(
+            prevPacketParser: errorCodePacketParser,
+            isAscii: plc.IsAscii,
+            wordLength: wordLength,
+            doubleWordLength: doubleWordLength
+        );
+    }
+
+    public int GetIndex()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetLength()
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] ParsePacket(byte[] bytes)
+    {
+        subHeaderPacketParser.ParsePacket(bytes);
+        errorCodePacketParser.ParsePacket(bytes);
+        
+        if (contentLengthPacketParser != null) 
+        {
+            contentPacketParser!.Length = BitConverter.ToUInt16(contentLengthPacketParser.ParsePacket(bytes)) - (ushort)errorCodePacketParser.GetLength();
+            return contentPacketParser.ParsePacket(bytes);
+        }
+
+        return [];
+    }
+}
